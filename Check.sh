deac8801b773d3042b02229a5227cb1271d8d111
Stone_WJF <475400550@qq.com>
2021-02-22 11:16:29 +0800

    Update Check.sh

--- Check.sh
+++ Check.sh
@@ -34,22 +34,64 @@ else
 fi
 
 if [ "$(grep 'install hfs /bin/true' /etc/modprobe.d/CIS.conf | wc -l)"  -ge 1 ];then
-	echo "------【通过】确保禁用hfs文件系统的挂载 "
+	echo "------【通过】确保禁用hfs文件系统的挂载"
 else
-	echo "------【未通过】确保禁用hfs文件系统的挂载 "
+	echo "------【未通过】确保禁用hfs文件系统的挂载"
 fi
 
 if [ "$(grep 'install hfsplus /bin/true' /etc/modprobe.d/CIS.conf | wc -l)"  -ge 1 ];then
-	echo "------【通过】确保禁用hfsplus文件系统的挂载 "
+	echo "------【通过】确保禁用hfsplus文件系统的挂载"
 else
-	echo "------【未通过】确保禁用hfsplus文件系统的挂载 "
+	echo "------【未通过】确保禁用hfsplus文件系统的挂载"
 fi
 
 if [ "$(grep 'install vfat /bin/true' /etc/modprobe.d/CIS.conf | wc -l)"  -ge 1 ];then
-	echo "------【通过】确保禁止挂载udf文件系统 "
+	echo "------【通过】确保禁止挂载udf文件系统"
 else
-	echo "------【未通过】确保禁止挂载udf文件系统 "
+	echo "------【未通过】确保禁止挂载udf文件系统"
 fi
+
+echo "------【（默认）未通过】确保tmp存在单独的分区"
+echo "------【（默认）未通过】确保var存在单独的分区"
+echo "------【（默认）未通过】确保/var/tmp存在单独的分区"
+
+echo -e "\n-------------------------------------------------------------------------"
+echo "1 初始配置 -> 1.2 配置软件更新----------------------------------"
+if [ "$(grep -i gpgcheck /etc/yum.conf | grep -v '^#' |awk '{print $2}')" = "1" ];then
+	echo "-----【通过】（1.2.3）确保gpgcheck已全局激活（与腾讯检测结果不同）"
+else
+	echo "-----【未通过】（1.2.3）确保gpgcheck已全局激活（与腾讯检测结果不同）"
+fi
+
+echo -e "\n-------------------------------------------------------------------------"
+echo "1 初始配置 -> 1.4 文件系统完整性检查----------------------------------"
+if["$(rpm -q aide | wc -l)" -ge 1];then
+	echo "【通过】（1.4.1）确保安装了AIDE"
+else
+	echo "【未通过】（1.4.1）确保安装了AIDE"
+fi
+
+if["$(grep -i aide /etc/crontab | wc -l)" -ge 1];then
+	echo "【通过】（1.4.2）确保定期检查文件系统完整性"
+else
+	echo "【未通过】（1.4.2）确保定期检查文件系统完整性"
+fi
+
+echo -e "\n-------------------------------------------------------------------------"
+echo "1 初始配置 -> 1.5 加固Boot设置----------------------------------"
+if["$(grep "^\s*GRUB2_PASSWORD" /boot/grub2/grub.cfg | wc -l)" -ge 1];then
+	echo "【通过】（1.5.1）确保设置了bootloader密码"
+else
+	echo "【未通过】（1.5.1）确保设置了bootloader密码"
+fi
+
+if["$(stat /boot/grub2/grub.cfg | grep 0600 | wc -l)" -ge 1];then
+	echo "【通过】（1.5.2）确保配置了bootloader配置的权限"
+else
+	echo "【未通过】（1.5.2）确保配置了bootloader配置的权限"
+fi
+
+echo "【（默认）未通过】确保设置了引导程序密码"
 
 echo -e "\n-------------------------------------------------------------------------"
 echo "1 初始配置 -> 1.6 额外的进程加固----------------------------------"
@@ -63,6 +105,19 @@ if [ "$(grep 'kernel.randomize_va_space = 2'  /etc/sysctl.conf | wc -l)"  -ge 1 
 	echo "------【通过】（1.6.3）确保启用了地址空间布局随机化（ASLR）"
 else
 	echo "------【未通过】（1.6.3）确保启用了地址空间布局随机化（ASLR）"
+fi
+
+echo -e "\n-------------------------------------------------------------------------"
+echo "1 初始配置 -> 1.8 警告标语----------------------------------"
+echo "【（默认）未通过】确保正确配置本地登录警告标语"
+echo "【（默认）未通过】确保正确配置远程登录警告标语"
+
+echo -e "\n-------------------------------------------------------------------------"
+echo "2 服务 -> 2.2 特殊目的的服务----------------------------------"
+if [ "$(systemctl status squid | grep 'active (running)' | wc -l)"  -lt 1 ];then
+	echo "------【通过】（2.2.14）确保未启用HTTP代理服务"
+else
+	echo "------【未通过】（2.2.14）确保未启用HTTP代理服务"
 fi
 
 echo -e "\n-------------------------------------------------------------------"
@@ -123,6 +178,12 @@ else
 	echo "------【未通过】（5.2.9）确保已禁用SSH基于主机的身份验证"
 fi
 
+if [ "$(grep -i PermitRootLogin /etc/ssh/sshd_config | grep -v '^#' |awk '{print $2}')" = "no" ];then
+	echo "------【通过】（5.2.10）确保禁用SSH root登录"
+else
+	echo "------【未通过】（5.2.10）确保禁用SSH root登录"
+fi
+
 if [ "$(grep -i PermitEmptyPasswords /etc/ssh/sshd_config | grep -v '^#' |awk '{print $2}')" = "no" ];then
 	echo "------【通过】（5.2.11）确保已禁用SSH空密码登录"
 else
@@ -151,7 +212,13 @@ else
 	echo "------【未通过】（5.2.17）确保SSH LoginGraceTime设置为一分钟或更短"
 fi
 
-
+echo -e "\n-------------------------------------------------------------------------"
+echo "3 网络配置 -> 3.1 禁用未使用的网络协议和设备----------------------------------"
+if [ "$(grep "^\s*linux" /boot/grub2/grub.cfg | grep -v ipv6.disable=1 | wc -l)"  -ge 1 ];then
+	echo "------【通过】（3.1.1）确保已禁用IPv6"
+else
+	echo "------【未通过】（3.1.1）确保已禁用IPv6"
+fi
 echo -e "\n-------------------------------------------------------------------------"
 echo "3 网络配置 -> 3.2 网络参数（仅限主机）----------------------------------"
 if [ "$(grep 'net.ipv4.ip_forward = 0'  /etc/sysctl.conf | wc -l)"  -ge 1 ];then
